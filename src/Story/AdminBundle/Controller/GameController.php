0a84ee6db0bcb29206b9e24cee414ef07ec6af3f
Walter Schweitzer <wschweitzer00@gmail.com>
2013-10-13 20:25:14 -0400

    added games controller

---
+++ src/Story/AdminBundle/Controller/GameController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Story\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class GameController extends Controller {
+	
+	public function indexAction() {
+		
+	}
+}
